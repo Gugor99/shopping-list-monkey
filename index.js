1eb6cd85dad47fba1af266e4769ba3111be290c8
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-11-21 10:53:22 +0100

    Update live server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-4c29f-default-rtdb.firebaseio.com/"
+    databaseURL: "https://groch-list-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
